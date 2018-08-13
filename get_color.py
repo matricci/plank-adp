ad437be058db1e1c54f90afb4cc6d453d1d89d66
Matheus Ricci <matricci2011@hotmail.com>
2018-08-13 20:52:18 -0300

    Comment the code

--- get_color.py
+++ get_color.py
@@ -5,19 +5,19 @@ import os
 import sys 
 import fileinput
 #
-filename = "wallpaper.jpg";
-image = Image.open( filename );
-width, height = image.size
+filename = "wallpaper.jpg"; ## Open image 
+image = Image.open( filename ); 
+width, height = image.size ## Get image size
+# Get 10% of bottom image
 num = height * 0.10
 num2 = round(num)
 num3 = height - num2
 dock = int(num3)
-print(0, dock, height, width)
-cropped_image = image.crop( ( 0, dock, width, height ) );
-img2 = cropped_image.resize((1, 1),Image.ANTIALIAS)
-color = img2.getpixel((0, 0))
-color2=('{};;{};;{};;235'.format(*color))
-print(color2)
+#
+cropped_image = image.crop( ( 0, dock, width, height ) ); ## Cut the image 
+img2 = cropped_image.resize((1, 1),Image.ANTIALIAS) ## Resize to 1x1, same as average 
+color = img2.getpixel((0, 0)) ## Get the color
+color2=('{};;{};;{};;235'.format(*color)) ## Format the color to replace inside 'dock.theme'
 #Here starts the replacement part of the code ;)
 f = open('base.theme','r')
 filedata = f.read()
