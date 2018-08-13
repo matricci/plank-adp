6f9fec36e5cb1539c3e1c6c72cd3b143b0cc8dc6
Matheus Ricci <matricci2011@hotmail.com>
2018-08-13 20:34:20 -0300

    Solve the problem with high resolution images

--- get_color.py
+++ get_color.py
@@ -7,10 +7,17 @@ import fileinput
 #
 filename = "wallpaper.jpg";
 image = Image.open( filename );
-cropped_image = image.crop( ( 0, 1240, 2560, 1440 ) );
+width, height = image.size
+num = height * 0.10
+num2 = round(num)
+num3 = height - num2
+dock = int(num3)
+print(0, dock, height, width)
+cropped_image = image.crop( ( 0, dock, width, height ) );
 img2 = cropped_image.resize((1, 1),Image.ANTIALIAS)
 color = img2.getpixel((0, 0))
 color2=('{};;{};;{};;235'.format(*color))
+print(color2)
 #Here starts the replacement part of the code ;)
 f = open('base.theme','r')
 filedata = f.read()
