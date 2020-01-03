5af68a65c3980c3dc3747f794f25848d6bbdc310
Matheus Ricci <matricci2011@hotmail.com>
2020-01-03 13:49:16 -0300

    Fix icon location

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,7 +3,7 @@
 path="/usr/share/plank-adp"
 sudo rm -rfv $path
 sudo rm -v /usr/share/applications/plank-adp.desktop
-sudo rm -v /usr/share/icons/hicolor/128x128/plank-adp.svg
+sudo rm -v /usr/share/icons/hicolor/128x128/apps/plank-adp.svg
 
 sudo update-desktop-database /usr/share/applications/
 sudo gtk-update-icon-cache /usr/share/icons/hicolor/ -t
