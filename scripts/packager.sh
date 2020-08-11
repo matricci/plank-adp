38d742524e04eac4827c3464ae8ef9cbf8b54ace
Matheus Ricci <matricci2011@hotmail.com>
2020-08-11 20:08:10 -0300

    [Build] Add clean stage to the packager

--- scripts/packager.sh
+++ scripts/packager.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+## Prepare the fakeroot tree ##
+rm -rf ../build/usr
 path="../build/usr/share/plank-adp"
 mkdir -p $path
 mkdir -v $path/ui
@@ -16,7 +19,12 @@ cp -v ../src/plank-adp.desktop ../build/usr/share/applications/
 cp -v ../ui/plank-adp.svg ../build/usr/share/icons/hicolor/128x128/apps
 chmod a=+rwx $path
 chmod 0655 ../build/usr/share/icons/hicolor/128x128/apps/plank-adp.svg
+## ====================  ##
+
+## Build the packages ##
 cd ..
+rm plank-adp*
 /usr/bin/dpkg-deb --build build
 mv build.deb plank-adp_1.0.1.deb
 fpm  -s deb -t rpm --no-auto-depends -d 'plank' -d 'python3-gobject' -d 'python3-pillow' -d 'python3-cairo' plank-adp_1.0.1.deb
+## ====================  ##
