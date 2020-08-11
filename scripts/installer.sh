f49aca2960d5a198402ff6c9d2c464735f4e066f
Matheus Ricci <matricci2011@hotmail.com>
2020-08-11 00:22:54 -0300

    [Fix] Code cleanup

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,35 +1,35 @@
-#!/bin/bash 
+#!/bin/bash
 
-if [ -e ~/.local/share/plank/themes/Wallpaper ]; then 
+if [ -e ~/.local/share/plank/themes/Wallpaper ]; then
 	echo "Theme diretory is present"
-else 
-	mkdir -v ~/.local/share/plank/themes/Wallpaper 
+else
+	mkdir -v ~/.local/share/plank/themes/Wallpaper
 fi
 #
 # Check depends
-if [ -e /usr/bin/python3 ]; then 
+if [ -e /usr/bin/python3 ]; then
 	echo "Python installed"
-else 
+else
 	echo "Install Python before try to run again!"
 	exit
 fi
-if [ -e /usr/bin/pip3 ] ; then 
+if [ -e /usr/bin/pip3 ]; then
 	echo "Python-pip installed!"
 else
 	echo "Please install python-pip"
 	exit
 fi
 
-if [ -e /usr/bin/dconf ] ; then
+if [ -e /usr/bin/dconf ]; then
 	echo "Dconf-tools installed!"
 else
 	echo "Please install dconf-tools"
 	exit
 fi
-# 
-# 
+#
+#
 # Install Packages
-pip3 install Pillow 
+pip3 install Pillow
 pip3 install pycairo
 pip3 install PyGobject
 
@@ -38,7 +38,7 @@ path="/usr/share/plank-adp"
 sudo mkdir -v $path
 sudo mkdir -v $path/ui
 sudo mkdir -v $path/bin
-sudo cp -v ../ui/form1.glade $path/ui 
+sudo cp -v ../ui/form1.glade $path/ui
 sudo cp -v ../src/main.py $path/bin
 sudo cp -v ../src/utils.py $path/bin
 sudo cp -v ../base.theme $path
@@ -51,7 +51,6 @@ sudo chmod 0655 /usr/share/icons/hicolor/128x128/apps/plank-adp.svg
 # Change the theme to "Wallpaper"
 dconf write /net/launchpad/plank/docks/dock1/theme "'Wallpaper'"
 
-# post-install 
+# post-install
 sudo update-desktop-database /usr/share/applications/
 sudo gtk-update-icon-cache /usr/share/icons/hicolor/ -t
-
