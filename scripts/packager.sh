a533a4fa183123a04c598bdd543e48ac8d4dd0ab
Matheus Ricci <matricci2011@hotmail.com>
2020-08-11 20:02:41 -0300

    [Build] Use fpm to build rpm packages

--- scripts/packager.sh
+++ scripts/packager.sh
@@ -7,7 +7,6 @@ mkdir -v $path/bin
 mkdir -p ../build/usr/bin/
 mkdir -p ../build/usr/share/applications/
 mkdir -p ../build/usr/share/icons/hicolor/128x128/apps
-mkdir -p ../build/usr/share/plank/themes/Wallpaper
 cp -v ../ui/form1.glade $path/ui
 cp -v ../src/main.py $path/bin
 cp -v ../src/utils.py $path/bin
@@ -17,4 +16,7 @@ cp -v ../src/plank-adp.desktop ../build/usr/share/applications/
 cp -v ../ui/plank-adp.svg ../build/usr/share/icons/hicolor/128x128/apps
 chmod a=+rwx $path
 chmod 0655 ../build/usr/share/icons/hicolor/128x128/apps/plank-adp.svg
-chmod 0655 ../build/usr/share/plank/themes/Wallpaper
+cd ..
+/usr/bin/dpkg-deb --build build
+mv build.deb plank-adp_1.0.1.deb
+fpm  -s deb -t rpm --no-auto-depends -d 'plank' -d 'python3-gobject' -d 'python3-pillow' -d 'python3-cairo' plank-adp_1.0.1.deb
