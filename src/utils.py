bb2884c8d4d6f1c9e18c78dee714b746254758e5
Matheus Ricci <matricci2011@hotmail.com>
2020-08-13 02:58:37 -0300

    [Fix] Resolves #3

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 from gi.repository import Gio
 
 GNOME_SCHEMA = 'org.gnome.desktop.background'
@@ -12,15 +13,17 @@ def get_desktop_environment():
 
 
 def check_theme_folder():
-    if(os.path.exists('/{}/.local/share/plank/themes/Wallpaper/'.format(os.environ.get('HOME'))) == False):
-        os.system("mkdir -p ~/.local/share/plank/themes/Wallpaper")
+    path = '/{}/.local/share/plank/themes/Wallpaper/'.format(
+        os.environ.get('HOME'))
+    if(os.path.exists(path) is False):
+        os.mkdir(path)kdir - p ~/.local/share/plank/themes/Wallpaper")
 
 
 def set_wallpaper(file):
-    env = get_desktop_environment()
+    env=get_desktop_environment()
     if env == "GNOME" or env == "Pantheon":
-        gsettings = Gio.Settings.new(GNOME_SCHEMA)
+        gsettings=Gio.Settings.new(GNOME_SCHEMA)
         gsettings.set_string(GNOME_KEY, file)
     elif env == "MATE":
-        gsettings = Gio.Settings.new("org.mate.background")
+        gsettings=Gio.Settings.new("org.mate.background")
         gsettings.set_string("picture-filename", file[7:])
